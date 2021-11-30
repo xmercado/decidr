dcb619bb97078227c11b36b32c67324b24a88163
Xavier Mercado <xmercado1576@gmail.com>
2021-11-30 12:51:46 -0800

    feat(app): import components and pass state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import Header from './components/Header';
+import AddItem from './components/AddItem';
+import SelectItem from './components/SelectItem';
+import ItemList from './components/ItemList';
 
 function App() {
   const [listContainer, setListContainer] = useState([]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ItemList
+        listContainer={listContainer}
+        setListContainer={setListContainer}
+      />
+      <AddItem 
+        listContainer={listContainer}
+        setListContainer={setListContainer}
+      />
+      <SelectItem />
     </div>
   );
 }
