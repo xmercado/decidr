808c10aba710658b60024d8a74104f6016a40d78
Xavier Mercado <xmercado1576@gmail.com>
2021-11-30 12:52:57 -0800

    feat(add-item): add input and button functions

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,9 +1,29 @@
 import React, { useState } from "react";
 
-export default function AddItem() {
+export default function AddItem({ listContainer, setListContainer }) {
+    const [newItem, setNewItem] = useState('');
+
+    const addItemToListContainer = (event) => {
+        event.preventDefault();
+        setListContainer([...listContainer, newItem]);
+        setNewItem('');
+    }
+
     return (
-        <div>
-            <p>AddItem</p>
-        </div>
+        <form>
+            <input 
+                id='add-item'
+                placeholder={'Add Item'}
+                value={newItem} 
+                onChange={event => setNewItem(event.target.value)}
+            />
+            <button
+                id='submit-item'
+                type='submit'
+                onClick={addItemToListContainer}
+            >
+                Add Item
+            </button>
+        </form>
     );
 }
