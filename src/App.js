8d1abf25952434f32b29652824037ab9547206e0
Xavier Mercado <xmercado1576@gmail.com>
2021-12-01 11:46:24 -0800

    feat(app): display selected item, add redo button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
 import Header from './components/Header';
 import AddItem from './components/AddItem';
 import SelectItem from './components/SelectItem';
@@ -9,27 +10,49 @@ function App() {
   const [listContainer, setListContainer] = useState([]);
   const [selectedItem, setSelectedItem] = useState('');
 
-  console.log('Selected item ', selectedItem);
+  const clearState = () => {
+    setListContainer([]);
+    setSelectedItem('');
+  }
 
   return (
     <div className="App">
       <Header />
-      <ItemList
-        listContainer={listContainer}
-        setListContainer={setListContainer}
-        selectedItem={selectedItem}
-        setSelectedItem={setSelectedItem}
-      />
-      <div>
-        <AddItem 
-          listContainer={listContainer}
-          setListContainer={setListContainer}
-        />
-        <SelectItem 
-          listContainer={listContainer}
-          setListContainer={setListContainer}
-          setSelectedItem={setSelectedItem}/>
-      </div>
+      {
+      selectedItem.length > 0
+        ?
+        <div className='Result'>
+          <p>Your decision is </p>
+          <p>{selectedItem}</p>
+          <div className='Pink'>
+            <Button
+              id='decide-again'
+              onClick={() => clearState()}
+            >
+              Decide again
+            </Button>
+          </div>
+        </div>
+        :
+        <>
+          <ItemList
+            listContainer={listContainer}
+            setListContainer={setListContainer}
+            selectedItem={selectedItem}
+            setSelectedItem={setSelectedItem}
+          />
+          <div>
+            <AddItem 
+              listContainer={listContainer}
+              setListContainer={setListContainer}
+            />
+            <SelectItem 
+              listContainer={listContainer}
+              setListContainer={setListContainer}
+              setSelectedItem={setSelectedItem}/>
+          </div>
+        </>
+      }
     </div>
   );
 }
