bf722b48791add317f640d8f70b8a5f404722c0b
Xavier Mercado <xmercado1576@gmail.com>
2021-11-30 17:34:25 -0800

    feat(item-list): add show and delete functions

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function ItemList() {
+export default function ItemList({ listContainer, setListContainer, selectedItem, setSelectedItem}) {
+    const deleteItem = () => {
+        setListContainer(listContainer.filter((value, index) => index !== selectedItem));
+        setSelectedItem('');
+    }
+
+    const displayListContainer = listContainer.map((item, index) => (
+        <div
+            className='ListItem'
+            key={index}
+        >
+            <p>
+                {item}
+            </p>
+            <div onClick={() => deleteItem()}>
+                <DeleteIcon />
+            </div>
+        </div>
+        )
+    );
+
     return (
         <div>
-            <p>ItemList</p>
+            {displayListContainer}
         </div>
     );
 }
