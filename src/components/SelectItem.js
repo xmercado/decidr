a02bb483eaab6d3fb8a171c25c538cd332fb42ea
Xavier Mercado <xmercado1576@gmail.com>
2021-11-30 17:34:51 -0800

    feat(select-item): add random choice function

--- src/components/SelectItem.js
+++ src/components/SelectItem.js
@@ -1,11 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
+import { Button } from '@material-ui/core';
 
-export default function SelectItem() {
+export default function SelectItem({ listContainer, setListContainer, setSelectedItem }) {
+    const selectRandomItem = () => {
+        const random = Math.floor(Math.random() * listContainer.length);
+        setSelectedItem(listContainer[random]);
+    }
+
     return (
-        <button
-            id='select-item'
-        >
-            Decide!
-        </button>
+        <div className='Pink'>
+            <Button
+                id='select-item'
+                onClick={selectRandomItem}
+            >
+                Decide!
+            </Button>
+        </div>
+
     );
 }
