68190065426980e9e28afe9c8e834650dbe7dc04
Xavier Mercado <xmercado1576@gmail.com>
2021-12-02 12:53:32 -0800

    feat(select): disable decide button based on items

--- src/components/SelectItem.js
+++ src/components/SelectItem.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
-export default function SelectItem({ listContainer, setListContainer, setSelectedItem }) {
+export default function SelectItem({ listContainer, setSelectedItem }) {
     const selectRandomItem = () => {
         const random = Math.floor(Math.random() * listContainer.length);
         setSelectedItem(listContainer[random]);
     }
 
     return (
-        <Button
-            className='Button'
-            id='select-item'
-            onClick={selectRandomItem}
-            variant='contained'
-            color='primary'
-        >
-            Decide!
-        </Button>
+        <>
+        {
+            listContainer.length < 2
+            ?
+                <Button
+                className='Button'
+                id='select-item'
+                variant='outlined'
+                color='primary'
+                >
+                    Decide!
+                </Button>
+            :
+                <Button
+                    className='Button'
+                    id='select-item'
+                    onClick={selectRandomItem}
+                    variant='contained'
+                    color='primary'
+                >
+                    Decide!
+                </Button>
+        }
+        </>
     );
 }
