56608363ec8cd179fd4077f5b5153ba0344667e0
Xavier Mercado <xmercado1576@gmail.com>
2021-12-02 16:01:44 -0800

    feat(header): add signature

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,12 @@ export default function Header() {
             <h2>
                 decidr
             </h2>
+            <a 
+                className='Signature'
+                href='https://github.com/xmercado'
+            >
+                * by Xavier Mercado
+            </a>
         </header>
     );
 }
